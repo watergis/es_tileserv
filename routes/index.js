50712c843ac3020598e74a63702606657faaad6e
Jin IGARASHI <wushilan0623@gmail.com>
2020-11-22 08:30:00 +0900

    added healthcheck api

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const wrap = fn => (...args) => fn(...args).catch(args[2])
-
-router.get('/tile/:z/:x/:y.:ext', wrap(require('./tile')));
+router.get('/healthcheck', wrap(require('./healthcheck')));
+router.get('/api/tile/:z/:x/:y.:ext', wrap(require('./tile')));
 
 module.exports = router;
